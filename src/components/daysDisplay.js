fc85a56fc0f8fe260c739f5f1fe09f6215c2228b
joaocostajs <joaocostajjsc@gmail.com>
2021-04-22 13:32:10 +0200

    math.round for min values

--- src/components/daysDisplay.js
+++ src/components/daysDisplay.js
@@ -5,7 +5,7 @@ export const DaysDisplay = ({item, k}) => {
                 <p style={{marginBottom:"1em", fontWeight:"900"}}>{String(new Date(item[k].dt * 1000)).split(" ")[0].toUpperCase()}</p>
                 </center>
                 <p > Max: {Math.round(Number(item[k].temp.max))}</p>
-                <p style={{marginBottom:"1em"}}> Min: {item[k].temp.min}</p>
+                <p style={{marginBottom:"1em"}}> Min:  {Math.round(Number(item[k].temp.min))}</p>
                 <center >
                     <div style={{width:"80%",display:"grid", gridTemplateColumns:"40px 1fr", alignItems:"center"}}>
                       <img style={{margin:"0", width: "40px"}} src={"http://openweathermap.org/img/w/" + item[k].weather[0].icon + ".png"} alt=""/>
